80fd77c496d6cb6e1e492088eab80250d18c9e48
fausto bischoff <fabischoff@yahoo.com.br>
2023-02-14 14:59:17 -0300

    Product

---
+++ src/main/java/br/com/infabis/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package br.com.infabis.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.infabis.course.entities.Product;
+import br.com.infabis.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
