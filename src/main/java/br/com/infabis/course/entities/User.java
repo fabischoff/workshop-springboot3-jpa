905df6519c42816a7ae928f4ec9f4f1cd0ae16c2
fausto bischoff <fabischoff@yahoo.com.br>
2023-02-14 12:09:00 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/br/com/infabis/course/entities/User.java
+++ src/main/java/br/com/infabis/course/entities/User.java
@@ -1,19 +1,22 @@
 package br.com.infabis.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_user") 
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -21,8 +24,12 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
-	public User() {}
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+	public User() {
+	}
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -73,6 +80,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -89,8 +100,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
+
 }
