9e8684c102518b2de00e4c9a6f6327bb5dbae678
fausto bischoff <fabischoff@yahoo.com.br>
2023-02-17 11:47:22 -0300

    Exception handling - update

--- src/main/java/br/com/infabis/course/services/UserService.java
+++ src/main/java/br/com/infabis/course/services/UserService.java
@@ -12,6 +12,7 @@ import br.com.infabis.course.entities.User;
 import br.com.infabis.course.repositories.UserRepository;
 import br.com.infabis.course.services.exceptions.DatabaseException;
 import br.com.infabis.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -43,9 +44,15 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateDate(entity, obj);
 		return repository.save(entity);
+			}
+		catch (EntityNotFoundException e) {
+			
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateDate(User entity, User obj) {
