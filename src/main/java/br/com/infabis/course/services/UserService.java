f92ab56e1933bfaeb1886c4b2fbcb551f8ddc442
fausto bischoff <fabischoff@yahoo.com.br>
2023-02-17 11:39:58 -0300

    Exception handling - delete

--- src/main/java/br/com/infabis/course/services/UserService.java
+++ src/main/java/br/com/infabis/course/services/UserService.java
@@ -3,12 +3,14 @@ package br.com.infabis.course.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.ResourceClosedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.infabis.course.entities.User;
 import br.com.infabis.course.repositories.UserRepository;
+import br.com.infabis.course.services.exceptions.DatabaseException;
 import br.com.infabis.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +33,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
@@ -41,7 +49,6 @@ public class UserService {
 	}
 
 	private void updateDate(User entity, User obj) {
-
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
